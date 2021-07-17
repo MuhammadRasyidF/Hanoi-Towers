b84b7a1d2386836852f2b1a1533f7824c18ab8e8
MuhammadRasyidF <rasyid201101@gmail.com>
2021-07-17 12:30:25 +0700

    adding boolean.h and stackHanoi.h

---
+++ stackHanoi.h
@@ -0,0 +1,81 @@
+//stackHanoi.h
+/*
+ *	File          : stackHanoi.h
+ *	Deskripsi     : Header file dari ADT Stack [Linked List]
+ *  Author        : Muhammad Rasyid Fadlurrahman - 201524019
+ *                  Gefi
+ *                  Salma 
+ */
+
+#ifndef stackHanoi_H
+#define stackHanoi_H
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include “boolean.h”
+
+#define Nil NULL
+
+#define Cakram(p) (p)->cakram
+#define Prev(p) p->prev
+
+#define top(s) (s).top
+
+/* Definisi Type Bentukan ADT Stack [Linked List] */
+
+typedef struct{
+int value;
+}infotype;
+
+typedef struct telmstack *Address;
+
+typedef struct telmstack{
+	infotype cakram;
+	Address prev;
+}elemen;
+
+typedef struct{
+	address top;
+}stack;
+
+/* Prototype ADT Stack [Linked List] */
+/* {Konstruktor pembentuk Stack} */
+void buatTower(stack *S);
+/* I.S : S terdefinisi, tidak diketahui nilainya
+   F.S : S diinisialisasi top(s) = nil,
+*/
+
+/* {Operasi terhadap komponen : selektor Get dan Set} */
+//Destruktor or Dealokator
+address alokasi(infotype X);
+//Mengirim sebuah elemen stack dalam bentuk address
+
+void dealokasi(address P);
+/* I.S : P terdefinisi
+   F.S : P dikembalikan ke sistem
+   		 Melakukan dealokasi / pengembalian address P ke sistem
+*/
+
+/* {Kelompok Interaksi Dengan I/O Device} */
+void masukCakram(stack *S, infotype X);
+/* I.S : S terdefinisi sembarang, mungkin kosong
+   F.S : S bertambah sebuah elemen sebagai elemen pertama
+*/
+
+void keluarCakram(stack *S, infotype *X);
+/* I.S : S terdefinisi, Stack tidak kosong
+   F.S : Menghapus elemen pada top(s)
+*/
+
+void printTower(stack S);
+/* I.S : S terdefinisi, mungkin kosong
+   F.S : Menampilkan seluruh elemen dalam stack ke layar dari mulai TOP sampai dengan bottom jika tidak kosong
+*/
+
+boolean isTowerEmpty(Stack S);
+/* I.S : S terdefinisi, mungkin kosong, mungkin penuh
+   F.S : mengembalikan nilai true apabila kosong dan false apabila tidak kosong.
+*/
+
+#endif
